188d214bd1f068d1aeefa55072045a4aa1a24a95
Sergei Baranov <mariosb84@mail.ru>
2023-06-18 12:16:29 +0300

    0. Spring MVC [#6877]

---
+++ src/main/java/ru/job4j/controller/IndexController.java
@@ -0,0 +1,10 @@
+package ru.job4j.controller;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+
+@ThreadSafe
+@Controller
+public class IndexController {
+
+}
