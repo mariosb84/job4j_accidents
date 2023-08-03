f94450ef56fbd52bec2b39e7ddaade5b96726204
Sergei Baranov <mariosb84@mail.ru>
2023-08-03 15:50:17 +0300

    0. Spring Security [#6879

---
+++ src/main/java/ru/job4j/security/SecurityInit.java
@@ -0,0 +1,7 @@
+package ru.job4j.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+
+}
