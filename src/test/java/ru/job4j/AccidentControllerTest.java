6a6d6e73749c8336ea992bfe90c22b28777f269b
Sergei Baranov <mariosb84@mail.ru>
2023-08-19 19:27:51 +0300

    0. Spring test [#6881 #436857]

--- src/test/java/ru/job4j/AccidentControllerTest.java
+++ src/test/java/ru/job4j/AccidentControllerTest.java
@@ -4,9 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.model.Accident;
+import ru.job4j.model.AccidentType;
+import ru.job4j.model.Rule;
+import ru.job4j.service.AccidentDataService;
 
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,6 +28,9 @@ public class AccidentControllerTest {
     @Autowired
     private MockMvc mockMvcAccident;
 
+    @MockBean
+    private AccidentDataService accidents;
+
     @Test
     @WithMockUser
     public void shouldReturnViewCreateAccident() throws Exception {
@@ -28,15 +40,17 @@ public class AccidentControllerTest {
                 .andExpect(view().name("/tasks/createAccident"));
     }
 
-     /*ЭТОТ ТЕСТ НЕ ПРОХОДИТ :*/
-
-  /* @Test
+   @Test
     @WithMockUser
     public void shouldReturnViewEditAccident() throws Exception {
-        this.mockMvcAccident.perform(get("/tasks/editAccident/{id}"))
+       var accident = new Accident(1, "Name", "Text",
+               "Address", new AccidentType(), Set.of(new Rule()));
+       when(accidents.findById(accident.getId())).thenReturn(Optional.of(accident));
+        this.mockMvcAccident.perform(get("/tasks/editAccident/{id}", 1))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("/tasks/editAccident"));
-    }*/
+    }
+
 
 }
